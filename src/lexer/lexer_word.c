e3659782778f9370bd1b8676ee4a905ba0e677cd
Rachid Jaada <rjaada42@gmail.com>
2025-02-18 15:25:06 +0100

    testing

--- src/lexer/lexer_word.c
+++ src/lexer/lexer_word.c
@@ -6,7 +6,7 @@
 /*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:26:22 by rjaada            #+#    #+#             */
-/*   Updated: 2025/02/18 15:22:31 by rjaada           ###   ########.fr       */
+/*   Updated: 2025/02/18 15:24:30 by rjaada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,6 @@ static int	process_quote_content(t_lexer *lexer, char quote_type, int *pos,
 	return (start);
 }
 
-static char	*get_quoted_content(t_lexer *lexer, char quote_type, int *pos)
-{
-	int		start;
-	int		len;
-	char	*content;
-
-	start = process_quote_content(lexer, quote_type, pos, &len);
-	if (start == -1)
-		return (NULL);
-	content = ft_substr(lexer->input, start, len);
-	return (content);
-}
-
 static char	*merge_words(char *word1, char *word2)
 {
 	char	*result;
@@ -58,23 +45,56 @@ static char	*merge_words(char *word1, char *word2)
 	return (result);
 }
 
-static char	*check_next_quote(t_lexer *lexer, char *current_word)
+static char	*get_quoted_content(t_lexer *lexer, char quote_type, int *pos)
 {
-	char	*next_word;
-	char	*temp;
+	int		start;
+	int		len;
+	char	*content;
 
-	while (lexer->pos < lexer->len && (lexer->input[lexer->pos] == '\''
-			|| lexer->input[lexer->pos] == '"'))
+	if (lexer->input[*pos] == quote_type && lexer->input[*pos
+		+ 1] == quote_type)
 	{
-		temp = handle_quote(lexer, lexer->input[lexer->pos])->value;
-		next_word = ft_strdup(temp);
-		free(temp);
-		if (!next_word)
+		*pos += 1;
+		return (ft_strdup(""));
+	}
+	start = process_quote_content(lexer, quote_type, pos, &len);
+	if (start == -1)
+		return (NULL);
+	content = ft_substr(lexer->input, start, len);
+	return (content);
+}
+
+static char	*check_next_token(t_lexer *lexer, char *current_word)
+{
+	char	quote_type;
+	char	*quoted_part;
+	int		original_pos;
+
+	while (lexer->pos < lexer->len)
+	{
+		original_pos = lexer->pos;
+		if (lexer->input[lexer->pos] == '\'' || lexer->input[lexer->pos] == '"')
 		{
-			free(current_word);
-			return (NULL);
+			quote_type = lexer->input[lexer->pos];
+			lexer->pos++;
+			quoted_part = get_quoted_content(lexer, quote_type, &lexer->pos);
+			if (!quoted_part)
+			{
+				free(current_word);
+				return (NULL);
+			}
+			current_word = merge_words(current_word, quoted_part);
+			lexer->pos++;
 		}
-		current_word = merge_words(current_word, next_word);
+		else if (is_word_char(lexer->input[lexer->pos]))
+		{
+			quoted_part = handle_word(lexer)->value;
+			current_word = merge_words(current_word, quoted_part);
+		}
+		else
+			break ;
+		if (lexer->pos == original_pos)
+			break ;
 	}
 	return (current_word);
 }
@@ -86,11 +106,6 @@ t_token	*handle_quote(t_lexer *lexer, char quote_type)
 	int		pos;
 
 	lexer->pos++;
-	if (lexer->input[lexer->pos] == quote_type)
-	{
-		lexer->pos++;
-		return (token_create(TOKEN_WORD, ft_strdup("")));
-	}
 	pos = lexer->pos;
 	word = get_quoted_content(lexer, quote_type, &pos);
 	if (!word)
@@ -99,13 +114,16 @@ t_token	*handle_quote(t_lexer *lexer, char quote_type)
 		return (NULL);
 	}
 	lexer->pos = pos + 1;
-	if (quote_type == '"' && ft_strchr(word, '$'))
+	if (quote_type == '"')
 	{
-		if (word[0] == '$' && !word[1])
-			return (token_create(TOKEN_WORD, word));
-		expanded = expand_shell_vars(word, lexer->env);
-		free(word);
-		return (token_create(TOKEN_WORD, expanded));
+		if (ft_strchr(word, '$'))
+		{
+			if (word[0] == '$' && !word[1])
+				return (token_create(TOKEN_WORD, word));
+			expanded = expand_shell_vars(word, lexer->env);
+			free(word);
+			return (token_create(TOKEN_WORD, expanded));
+		}
 	}
 	return (token_create(TOKEN_WORD, word));
 }
@@ -123,7 +141,7 @@ t_token	*handle_word(t_lexer *lexer)
 	word = ft_substr(lexer->input, start, lexer->pos - start);
 	if (!word)
 		return (NULL);
-	final_word = check_next_quote(lexer, word);
+	final_word = check_next_token(lexer, word);
 	if (!final_word)
 		return (NULL);
 	if (final_word[0] == '$' && !final_word[1])
